463d8f01701b072affe33645515239d0c90036e6
Sharky <114203428+UWU-Sharky@users.noreply.github.com>
2025-11-03 18:27:54 +0700

    Add files via upload

--- UnixLab2/script.sh
+++ UnixLab2/script.sh
@@ -3,49 +3,36 @@
 SHARED_DIR="$(pwd)/shared"
 LOCK_FILE="$SHARED_DIR/.lock"
 CONTAINER_ID=$(hostname)
-COUNTER=1
 
 mkdir -p "$SHARED_DIR"
 
-find_next_available() {
-    local i=1
-    while [[ $i -le 99 ]]; do
-        echo "$i"
-        num=$(printf "%03d" "$i")
-        if [ ! -e "$SHARED_DIR/$num" ]; then
-            echo $num
-            return 0
-        else ((i++))
-        fi
-        
-    done
-}
+while true; do
 
+    filename=""
 
-until [[ $COUNTER  -eq 99 ]]; do
     (
     flock -x 200
-        filename=$(find_next_available)
-        echo "$CONTAINER_ID $filename" > "$SHARED_DIR/$filename"
-        echo "Created: $filename"
+        for i in $(seq -f "%03g" 1 100); do
+            if [[ ! -f "$SHARED_DIR/$i" ]]; then
+                filename="$i"
+                echo "$CONTAINER_ID $filename" > "$SHARED_DIR/$filename"
+                cat "$SHARED_DIR/$filename"
+                break
+            fi
+        done
     ) 200>"$LOCK_FILE"
 
 
-    sleep 1
+    sleep 2
 
     if [[ -f "$SHARED_DIR/$filename" ]]; then
+       (
+        flock -x 200
         rm -f "$SHARED_DIR/$filename"
         echo "Deleted: $filename"
+        ) 200>"$LOCK_FILE"
     fi
+
+    sleep 2
     
-    ((COUNTER++))
-
-done
-
-
-
-files=("$SHARED_DIR"/*)
-for i in "${files[@]}"; do
-    echo $i
-    rm -rf "$i"
 done
